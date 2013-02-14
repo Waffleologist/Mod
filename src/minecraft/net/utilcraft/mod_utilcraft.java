bb430b88432c0c1805e039ff8621d464efde946f
Andrew Hazelbaker <spartan.hazelbaker76@gmail.com>
2013-02-13 20:41:17 -0800

    Added a potato chip
    I added a potato chip food. It restores 1 heart (subject to change) and
    is created when a potato is put in the furnace (Also subject to change)
    also added some generation and register code lines to base other items
    off of.

--- src/minecraft/net/utilcraft/mod_utilcraft.java
+++ src/minecraft/net/utilcraft/mod_utilcraft.java
@@ -1,19 +1,45 @@
 package net.utilcraft;
 
-
-//I AM AWESOME
-
-//blarg lawl
-
-
-
-
-
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.client.MinecraftForgeClient;
 
 
 
 
 
+
+
+
+
+
+
+
+//main class
 public class mod_utilcraft {
-
+	public static Item PotatoChip;
+	
+	public void load(FMLInitializationEvent event){
+	//add the proper textures
+	MinecraftForgeClient.preloadTexture("/UtilCraft/PotatoChip.png");
+		
+		
+		
+	//set the in game names
+	LanguageRegistry.addName(PotatoChip, "Potato Chip");
+	
+	
+	
+	//tell the game to register the recipe
+	GameRegistry.addSmelting(PotatoChip.itemID, new ItemStack(PotatoChip,1), 1f);
+	
+	}
+static {
+	//declare the item and its attributes
+	PotatoChip = (new ItemFood(1000, 1, 4F, true) .setIconIndex(1) .setItemName("Potato Chip"));
+}
 }
