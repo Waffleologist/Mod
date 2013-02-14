07dbd36d6f60e95fd6daeacafe5dd03d38ab214e
Andrew Hazelbaker <spartan.hazelbaker76@gmail.com>
2013-02-14 07:31:40 -0800

    Same code, just new class file and stuff
    Had to re-decompile and change a few things, same code though

--- src/minecraft/net/utilcraft/mod_utilcraft.java
+++ src/minecraft/net/utilcraft/mod_utilcraft.java
@@ -1,45 +1,30 @@
 package net.utilcraft;
 
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.registry.GameRegistry;
-import cpw.mods.fml.common.registry.LanguageRegistry;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.MinecraftForgeClient;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
 
-
-
-
-
-
-
-
-
-
-
-
-//main class
-public class mod_utilcraft {
+public class Mod_UtilCraft {
 	public static Item PotatoChip;
-	
-	public void load(FMLInitializationEvent event){
-	//add the proper textures
-	MinecraftForgeClient.preloadTexture("/UtilCraft/PotatoChip.png");
 		
+		public void load(FMLInitializationEvent event){
 		
+		//add the proper textures
+   
+	    MinecraftForgeClient.preloadTexture("/UtilCraft/PotatoChip.png");
+	    //set the in game names	LanguageRegistry.addName(PotatoChip, "Potato Chip");	
 		
-	//set the in game names
-	LanguageRegistry.addName(PotatoChip, "Potato Chip");
-	
-	
-	
-	//tell the game to register the recipe
-	GameRegistry.addSmelting(PotatoChip.itemID, new ItemStack(PotatoChip,1), 1f);
-	
+		//tell the game to register the recipe
+		GameRegistry.addSmelting(PotatoChip.itemID, new ItemStack(PotatoChip,1), 1f);
+		
+		}
+	    static {
+		//declare the item and its attributes
+		PotatoChip = (new ItemFood(1000, 1, 4F, true) .setIconIndex(1) .setItemName("Potato Chip"));
 	}
-static {
-	//declare the item and its attributes
-	PotatoChip = (new ItemFood(1000, 1, 4F, true) .setIconIndex(1) .setItemName("Potato Chip"));
 }
-}
+	
+
